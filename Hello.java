ff695b08f4330f81d6395b7017e704a1dd0968ef
anikman <anikman98@gmail.com>
2025-01-09 23:25:14 +0530

    added another println with more text

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ class Hello extends Object{
     public static void main(String args[]){
         System.out.println("Hello world!");
         System.out.println("I'm using jenkins to run this!");
+        System.out.println("This looks easy!!! Let's see what's ahead...");
     }
 }
